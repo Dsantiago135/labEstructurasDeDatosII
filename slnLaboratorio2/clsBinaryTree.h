1820907ffdc9f72f7b6bbd01b78113782f01ca1a
Dsantiago135 <mestradavid20@hotmail.com>
2024-08-28 11:47:44 -0500

    ._.

--- slnLaboratorio2/clsBinaryTree.h
+++ slnLaboratorio2/clsBinaryTree.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+namespace libBinaryTree {
+
+	template <class T>
+	class clsBinaryTree {
+
+	};
+}
--- slnLaboratorio2/slnLaboratorio2.cpp
+++ slnLaboratorio2/slnLaboratorio2.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "clsBinaryTree.h"
+
+using std::cout;
+using std::endl;
+
+using libBinaryTree::clsBinaryTree;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    clsBinaryTree<int> a;
 }
