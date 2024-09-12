68084eb3c3bc1b70c4d09ab950c99735ff225fb5
Dsantiago135 <mestradavid20@hotmail.com>
2024-09-11 19:03:21 -0500

    ._.

--- libCollectionII/ADTBinaryExpressionTree.h
+++ libCollectionII/ADTBinaryExpressionTree.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+
+namespace libBinaryTree {
+	template <class T>
+	/*@brief Árbol binario de Expresiones(es una subclase de árbol binario)*/
+	class clsBinaryExpressionTree : protected ADTBinaryTree {
+#pragma region Operations
+	protected:
+		bool opInsert(strNode* prmDadNode, strNode* prmNewNode) override {
+		}
+		void opRemove(strNode* prmNode, T prmData) override {
+		}
+	public:
+		void opInsert(T prmData) {
+		}
+		void opRemove(T prmData) {
+		}
+#pragma endregion
+	};
+}
