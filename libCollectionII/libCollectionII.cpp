7268a2ef01e2b76634f0b5b36e00cf41883920ac
Dsantiago135 <mestradavid20@hotmail.com>
2024-09-18 13:51:35 -0500

    ._.

--- libCollectionII/libCollectionII.cpp
+++ libCollectionII/libCollectionII.cpp
@@ -5,32 +5,32 @@ using std::cout;
 using std::endl;
 using  libBinaryTree::clsBinarySearchTree; 
 
-	int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
 
-		//Crear una instancia de arbol binario
-		clsBinarySearchTree<int> varTree;
+	//Crear una instancia de arbol binario
+	clsBinarySearchTree<int> varTree;
 
-		cout << (varTree.opItsEmpty() ? "El arbol esta vacio" : "El arbol no esta vacio") << endl;
+	cout << (varTree.opItsEmpty() ? "El arbol esta vacio" : "El arbol no esta vacio") << endl;
 
-		cout << "Insertar datos" << endl;
-		//Insertar datos
-		varTree.opInsert(30);
-		varTree.opInsert(25);
-		varTree.opInsert(36);
-		varTree.opInsert(47);
-		varTree.opInsert(32);
-		varTree.opInsert(27);
+	cout << "Insertar datos" << endl;
+	//Insertar datos
+	varTree.opInsert(30);
+	varTree.opInsert(25);
+	varTree.opInsert(36);
+	varTree.opInsert(47);
+	varTree.opInsert(32);
+	varTree.opInsert(27);
 
-		cout << (varTree.opItsEmpty() ? "El arbol esta vacio" : "El arbol no esta vacio") << endl;
-		//Imprimir en preorden
-		varTree.opShowPreOrder();
-		cout << endl;
+	cout << (varTree.opItsEmpty() ? "El arbol esta vacio" : "El arbol no esta vacio") << endl;
+	//Imprimir en preorden
+	varTree.opShowPreOrder();
+	cout << endl;
 
-		varTree.opShowInOrder();
-		cout << endl;
+	varTree.opShowInOrder();
+	cout << endl;
 
-		varTree.opShowPosOrder();
-		cout << endl;
+	varTree.opShowPosOrder();
+	cout << endl;
 
-		return 0;
-	};
+	return 0;
+};
